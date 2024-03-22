a8ba51e1a03fdd60d606e5bacafa53c3fbd90cd7
Zaineb Marediya <zmarediya@yahoo.com>
2024-03-21 21:14:30 -0500

    chapter 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>    </html>
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
   );
 }
