0dac7591c50d4882644141d66b72bd91da3bb243
Zaineb Marediya <zmarediya@yahoo.com>
2024-03-26 19:06:39 -0500

    chapter 11

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useDebouncedCallback } from 'use-debounce';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
@@ -8,7 +9,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`searching... + ${term}`);
     const params = new URLSearchParams(searchParams);
 
     if (term) {
@@ -19,7 +21,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
     replace(`${pathname}?${params.toString()}`);
 
-  }
+  }, 300)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
